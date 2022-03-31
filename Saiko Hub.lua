8596ab20394f1fa0d051ca79082a7f541fd9d01f
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-03-31 15:55:13 +0200

    Add files via upload

---
+++ Saiko Hub.lua
@@ -0,0 +1,86 @@
+while wait() do
+local args = {
+    [1] = "StoredDF1"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+
+local args = {
+    [1] = "StoredDF2"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF3"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF4"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF5"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF6"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF7"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF8"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF9"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF10"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF11"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+local args = {
+    [1] = "StoredDF12"
+}
+
+workspace.UserData.User_420935986.StoredDFRequest:FireServer(unpack(args))
+
+
+wait(10.5)
+
+
+while wait() do
+local args = {
+    [1] = "NOPLS"
+}
+
+game:GetService("Players").LocalPlayer.Character.Drown:FireServer(unpack(args))
+end
+end
