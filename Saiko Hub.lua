50c16d88a3077582de2e8e73a1cd8db235669f42
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-05-11 11:39:59 +0200

    Update Saiko Hub.lua

--- Saiko Hub.lua
+++ Saiko Hub.lua
@@ -20,7 +20,7 @@ end
 end)
 game.StarterGui:SetCore("SendNotification", {
 Title = "Saiko Hub"; -- the title (ofc)
-Text = "3.4"; -- what the text says (ofc)
+Text = "3.5"; -- what the text says (ofc)
 --Icon = "rbxassetid://57254792"; -- the image if u want. 
 Duration = 1; -- how long the notification should in secounds
 })
