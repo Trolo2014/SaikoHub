b3b7c3455375a539b85751e73268378bbd091390
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-05-22 22:53:55 +0200

    Update Saiko Hub.lua

--- Saiko Hub.lua
+++ Saiko Hub.lua
@@ -11,7 +11,7 @@ end
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Wave Hub"; -- the title (ofc)
-Text = "version 5.1"; -- what the text says (ofc)
+Text = "version 5.2"; -- what the text says (ofc)
 --Icon = "rbxassetid://57254792"; -- the image if u want. 
 Duration = 1; -- how long the notification should in secounds
 })
